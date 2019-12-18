07d7f7fca9a0c3ef109277e829c780b682659bee
msunilhyd <msunilhyd@gmail.com>
2019-12-18 03:16:11 -0800

    18-Dec-19 : videos home

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    vid_url = models.TextField(null=True)
 
     def __str__(self):
         return self.title
