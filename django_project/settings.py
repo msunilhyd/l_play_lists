e03fa0042e7d8d1c5510c636a97c5946006f7ed8
msunilhyd <msunilhyd@gmail.com>
2019-12-18 03:52:52 -0800

    18-Dec-19 : site logo

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.recordingcompany.in']
+ALLOWED_HOSTS = ['127.0.0.1', 'www.recordingcompany.in']
 
 # Application definition
 
