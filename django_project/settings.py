aaaaa17a9d004e6b12255927b6c88a102c12a2fc
msunilhyd <msunilhyd@gmail.com>
2019-12-23 23:27:21 -0800

    22-Dec-19 : added the media folder

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -140,4 +141,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'teamgradematrix@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('sudheer123')
+EMAIL_HOST_PASSWORD = 'sudheer123'
