2d4784c8c8e2af5656a942fad7b24f6d3646bd76
msunilhyd <msunilhyd@gmail.com>
2020-01-13 12:55:28 +0530

    13-Jan-20 : Removed the labels from the Register form

--- users/forms.py
+++ users/forms.py
@@ -7,9 +7,16 @@ from .models import Profile
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
 
 
 class UserUpdateForm(forms.ModelForm):
